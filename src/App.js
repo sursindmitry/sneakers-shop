e05cc6ee128e3443963ef9799e2cb06f67d4e457
sursindmitry <dmitrijsursin@gmail.com>
2023-03-21 15:48:10 +0700

    Header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="wrapper">
+            <header className="clear">
+                <div className="headerLeft">
+                    <img width={40} height={40} src="/img/logo.svg"/>
+                    <div>
+                        <h3>Sneakers shop</h3>
+                        <p>Магазин лучших кросовок</p>
+                    </div>
+                </div>
+
+                <ul className="headerRight">
+                    <li>
+                        <img width={18} height={18} src="/img/cart.svg"/>
+                        <span>1205 руб.</span>
+                    </li>
+                    <li>
+                        <img width={18} height={18} src="/img/user.svg"/>
+                    </li>
+                </ul>
+            </header>
+            <div className="content">
+                <h1>Все кроссовки</h1>
+                ...
+            </div>
+        </div>
+    );
 }
 
 export default App;
